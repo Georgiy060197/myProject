34054d97eba0e8d4c848807429cdb58043cf6ca8
Georgiy Truseev <igor.truseev@mail.ru>
2025-03-05 22:25:17 +0300

    выполнение задания  lesson02

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let title = 3;
-const screens = 123;
-let screenPrise = 10;
-const rollback = 0;
-let fullPrice = 40;
-const adaptive = 2;
+let title = "myProject"
+let screens = "Простые, Сложные, Интерактивные"
+const screenPrice = 13
+const rollback = 30
+let fullPrice = 1000
+const adaptive = true
 
-alert("Hello World!");
-console.log("Hello!!!")
+console.log(typeof title)
+console.log(typeof fullPrice)
+console.log(typeof adaptive)
+console.log(screens.length)
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей/ долларов/гривен/юани")
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей/ долларов/гривен/юани")
+console.log(screens.toLowerCase())
+console.log(fullPrice * (rollback / 100))
