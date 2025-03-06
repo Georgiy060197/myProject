732355b16cb7927243fb5eaa292f88f9c5667c16
Georgiy Truseev <igor.truseev@mail.ru>
2025-03-06 18:13:35 +0300

    внес поправки в script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let title = "myProject"
-let screens = "Простые, Сложные, Интерактивные"
+const title = "myProject"
+const screens = "Простые, Сложные, Интерактивные"
 const screenPrice = 13
 const rollback = 30
-let fullPrice = 1000
+const fullPrice = 1000
 const adaptive = true
 
 console.log(typeof title)
 console.log(typeof fullPrice)
 console.log(typeof adaptive)
 console.log(screens.length)
-console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей/ долларов/гривен/юани")
-console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей/ долларов/гривен/юани")
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "долларов")
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "долларов")
 console.log(screens.toLowerCase())
 console.log(fullPrice * (rollback / 100))
