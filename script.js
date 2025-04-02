e94a4731004fb92d85414f9d6822050d7847499c
Georgiy Truseev <igor.truseev@mail.ru>
2025-04-02 22:05:43 +0300

    Выполнение задания с правками к lesson08

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 'use strict'
-// const rollback = 30.5
-// let title
-// let screens
-// let screenPrice
-// let serviceQuestion1
-// let servicePrice1
-// let serviceQuestion2
-// let servicePrice2
-//const adaptive = confirm("Нужен ли адаптив на сайте?", "Да, нет")
+
 const appData = {
     rollback: 30.5,
     title: "",
@@ -26,12 +18,6 @@ const appData = {
     },
     asking: function () {
         appData.title = prompt("Как называется Ваш проект?", "Hello")
-        // appData.screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные")
-        // do {
-        //     appData.screenPrice = prompt("Сколько будет стоить данная работа?")
-        // }
-        // while (!appData.isNumber(appData.screenPrice))
-        // appData.screenPrice = Number(appData.screenPrice)
 
         for (let i = 0; i < 2; i++) {
             let name = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные")
@@ -59,25 +45,11 @@ const appData = {
         }
     },
 
-    //         appData.serviceQuestion1 = prompt("Какой дополнительный тип услуги нужен?")
-    //         do {
-    //             appData.servicePrice1 = prompt("Сколько это будет стоить?")
-    //         }
-    //         while (!appData.isNumber(appData.servicePrice1))
-    //         appData.servicePrice1 = Number(appData.servicePrice1)
-    //         appData.serviceQuestion2 = prompt("Какой дополнительный тип услуги нужен?")
-    //         do {
-    //             appData.servicePrice2 = prompt("Сколько это будет стоить?")
-    //         }
-    //         while (!appData.isNumber(appData.servicePrice2))
-    //         appData.servicePrice2 = Number(appData.servicePrice2)
-    //     },
-
     getAllServicePrices: function () {
         for (let key in appData.services) {
             appData.allServicePrices += appData.services[key]
         }
-        //appData.allServicePrices = appData.servicePrice1 + appData.servicePrice2
+
     },
     getFullPrice: function () {
         appData.fullPrice = appData.screenPrice + appData.allServicePrices
@@ -119,11 +91,6 @@ const appData = {
         appData.getRollbackMessage(appData.fullPrice)
         console.log(appData.screens)
         console.log(appData.screenPrice)
-        //         console.log(appData.allServicePrices)
-        //         console.log(appData.fullPrice)
-        //         console.log(appData.getTitle())
-        //         console.log(appData.servicePercentPrice)
-        //         appData.getRollbackMessage(appData.fullPrice)
         appData.logger()
     },
     logger: function () {
